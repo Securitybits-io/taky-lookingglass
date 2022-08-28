7ed08a4df224cdaa0eba1cdb308fe62c2f5eebe8
Christoffer Claesson <Christoffer.Claesson@securitybits.io>
2022-08-28 13:08:42 +0200

    added a simple logging factory

--- src/connect.py
+++ src/connect.py
@@ -6,6 +6,7 @@ import pymysql
 import ssl
 import xmltodict
 import certutil
+import logging
 
 from time import sleep
 from datetime import datetime
@@ -28,8 +29,11 @@ SCRT = certutil.CERT_PATH + "/server.crt"
 CCRT = certutil.BOT_CERT_PATH + "/taky-connect.crt"
 CKEY = certutil.BOT_CERT_PATH + "/taky-connect.key"
 
+LOG_LEVEL = os.getenv('LOG_LEVEL', "INFO").upper()
+
+
 def getCOT(socket, queue):
-  print('[+] Producer Thread started, waiting on CoTs...')
+  logging.info('[+] Producer Thread started, waiting on CoTs...')
 
   while(True):
     rawcot = socket.recv()
@@ -39,11 +43,11 @@ def getCOT(socket, queue):
 def sql_configure(conn):
   cursor = conn.cursor()
   
-  print('[+] Create CoTs table if not exists...')
+  logging.info('[+] Create CoTs table if not exists...')
   cursor.execute('CREATE TABLE IF NOT EXISTS cots (cot_id INT AUTO_INCREMENT PRIMARY KEY, time DATETIME NOT NULL, callsign VARCHAR(24), cot_group VARCHAR(10), role VARCHAR(16), lat DOUBLE, lon DOUBLE);')
   conn.commit()
   
-  print('[+] Create Grafana Read-Only User if not exist...')
+  logging.info('[+] Create Grafana Read-Only User if not exist...')
   cursor.execute('GRANT SELECT ON TAKYCoT.* to "grafana" IDENTIFIED BY "9AUZaNACtnc2TFcDups8euUy";')
   conn.commit()
 
@@ -53,7 +57,7 @@ def sql_configure(conn):
 
 
 def postCOT(sql, queue):
-  print('[+] Consumer Thread started, waiting on queued CoTs...')
+  logging.info('[+] Consumer Thread started, waiting on queued CoTs...')
   sql_insert = "INSERT INTO `cots` (`time`, `callsign`, `cot_group`, `role`, `lat`, `lon`) VALUES (%s, %s, %s, %s, %s, %s)"
 
   cursor = sql.cursor()
@@ -62,18 +66,20 @@ def postCOT(sql, queue):
       sleep(5)
     else:
       row = queue.get()
-      cot = parse_cot(row)
+      cot = parse_cot(row)´
+      
+      logging.debug("Raw CoT: %s", row)
+      logging.debug("Parsed CoT: %s", cot)
 
       cursor.execute(sql_insert, (cot['time'], cot['callsign'], cot['tak_color'], cot['tak_role'], cot['lat'], cot['lon']))
       sql.commit()
 
       sleep(0.5)
-      #print(cot)
-
+      
 
 def parse_cot(rawcot):
   dict_cot = xmltodict.parse(rawcot)
-  #print(dict_cot)
+  logging.debug(dict_cot)
   
   if ('a-f-G-U-C' in dict_cot['event']['@type']):
     detail = dict_cot['event']['detail']
@@ -97,11 +103,13 @@ def parse_cot(rawcot):
     }
 
     log = '{} | {} | {} | {} | {} | {}'.format(time, callsign, color, role, coords['@lat'], coords['@lon'])
-    print(log)
+    logging.debug("Formatted CoT: %s"log)
   return cot
 
 
 def main():
+  logging.basicConfig(format='%(levelname)s:%(threadName)s:%(message)s', level=LOG_LEVEL)
+
   queue = Queue()
   certutil.build_certs(cert_pass)
 
@@ -109,7 +117,7 @@ def main():
   sock_ssl = ssl.wrap_socket(sock, ca_certs=SCRT, cert_reqs=ssl.CERT_NONE, certfile=CCRT, keyfile=CKEY)
   conn = sock_ssl.connect((IP, PORT))
   if (sock_ssl._connected == True):
-    print("[+] Connected to TAKY")
+    logging.info("[+] Connected to TAKY")
     producer = Thread(target=getCOT, args=(sock_ssl, queue))
     producer.start()
 
